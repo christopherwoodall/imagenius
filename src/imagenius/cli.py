6687d54bca098eff19cc1ea16696d2f1a1cb07ee
Chris W <363708+0xchrisw@users.noreply.github.com>
2023-10-30 21:16:43 -0500

    Update cli.py

--- src/imagenius/cli.py
+++ src/imagenius/cli.py
@@ -1,13 +1,20 @@
-from flask import Flask, request, jsonify, render_template
-import requests
-from transformers import DetrImageProcessor, DetrForObjectDetection
-import torch
-from PIL import Image
-import requests
 from pathlib import Path
+
+import torch
+import requests
+
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+from transformers import DetrImageProcessor, DetrForObjectDetection
+from PIL import Image
+
+
+__name__ = 'imagenius'
 
 
 app = Flask(__name__, template_folder="./www")
+CORS(app) # https://flask-cors.readthedocs.io/en/latest/
 
 MODEL_PORT = 5000
 
@@ -21,7 +28,7 @@ model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revisi
 
 
 def detect():
-    config = 0.8
+    confidence = 0.8
 
     inputs = processor(images=image, return_tensors="pt")
     outputs = model(**inputs)
@@ -29,20 +36,43 @@ def detect():
     target_sizes = torch.tensor([image.size[::-1]])
     results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=confidence)[0]
 
+    client_results = []
     for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
         box = [round(i, 2) for i in box.tolist()]
-        print(
-                f"Detected {model.config.id2label[label.item()]} with confidence "
-                f"{round(score.item(), 3)} at location {box}"
+        client_results.append(
+            {
+                "label": model.config.id2label[label.item()],
+                "score": round(score.item(), 3),
+                "box": box,
+            }
         )
+        # print(
+        #         f"Detected {model.config.id2label[label.item()]} with confidence "
+        #         f"{round(score.item(), 3)} at location {box}"
+        # )
+
+    print(client_results)
+
+    return client_results
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-# @app.route('/upload', methods=['POST'])
+@app.route("/search")
+def search():
+    query = request.args.get("query")
+
+    results = detect()
+    print(results)
+    # detection = detect(query)
+
+    return jsonify(results)
+
+
+# @app.route("/upload", methods=["POST"])
 # def upload_file():
 #     file = request.files["file"]
 #     # Add logic to send this file to ML Model Server
@@ -52,4 +82,13 @@ def home():
 
 
 def main():
-    app.run(debug=True, port=6000)
+    app.run(
+        host="0.0.0.0",
+        port=6000,
+        ssl_context="adhoc",
+        debug=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
